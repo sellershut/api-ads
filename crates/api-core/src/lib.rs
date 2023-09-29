acbd013ab5ea9e6347acf14a10e8df88dec39d9c
rtkay123 <dev@kanjala.com>
2023-09-29 23:30:11 +0200

    add: de/serialise enum impl and serde proto tests

--- crates/api-core/src/lib.rs
+++ crates/api-core/src/lib.rs
@@ -1,14 +1,77 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{fmt::Formatter, marker::PhantomData};
+
+use protobuf::{EnumFull, EnumOrUnknown};
+
+include!(concat!(env!("OUT_DIR"), "/protos/mod.rs"));
+
+#[allow(dead_code)]
+fn serialize_enum_or_unknown<E: EnumFull, S: serde::Serializer>(
+    e: &Option<EnumOrUnknown<E>>,
+    s: S,
+) -> Result<S::Ok, S::Error> {
+    if let Some(e) = e {
+        match e.enum_value() {
+            Ok(v) => s.serialize_str(v.descriptor().name()),
+            Err(v) => s.serialize_i32(v),
+        }
+    } else {
+        s.serialize_unit()
+    }
+}
+
+#[allow(dead_code)]
+fn deserialize_enum_or_unknown<'de, E: EnumFull, D: serde::Deserializer<'de>>(
+    d: D,
+) -> Result<Option<EnumOrUnknown<E>>, D::Error> {
+    struct DeserializeEnumVisitor<E: EnumFull>(PhantomData<E>);
+
+    impl<'de, E: EnumFull> serde::de::Visitor<'de> for DeserializeEnumVisitor<E> {
+        type Value = Option<EnumOrUnknown<E>>;
+
+        fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
+            write!(formatter, "a string, an integer or none")
+        }
+
+        fn visit_str<R>(self, v: &str) -> Result<Self::Value, R>
+        where
+            R: serde::de::Error,
+        {
+            match E::enum_descriptor().value_by_name(v) {
+                Some(v) => Ok(Some(EnumOrUnknown::from_i32(v.value()))),
+                None => Err(serde::de::Error::custom(format!(
+                    "unknown enum value: {}",
+                    v
+                ))),
+            }
+        }
+
+        fn visit_i32<R>(self, v: i32) -> Result<Self::Value, R>
+        where
+            R: serde::de::Error,
+        {
+            Ok(Some(EnumOrUnknown::from_i32(v)))
+        }
+
+        fn visit_unit<R>(self) -> Result<Self::Value, R>
+        where
+            R: serde::de::Error,
+        {
+            Ok(None)
+        }
+    }
+
+    d.deserialize_any(DeserializeEnumVisitor(PhantomData))
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::category::Category;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn serialise_category_proto() {
+        let mut category = Category::new();
+        category.name = "foo".to_string();
+        category.id = "bar".to_string();
+        assert!(serde_json::to_string(&category).is_ok());
     }
 }
