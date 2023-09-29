1e2d80f8eab83fa5847268459e0e4da01a8bebf2
rtkay123 <dev@kanjala.com>
2023-09-29 22:35:32 +0200

    feat: start web server

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,11 +1,31 @@
+use std::net::SocketAddr;
+
+use axum::{routing::get, Router};
+use tracing::info;
+
+mod io;
 mod obsrv;
 
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     dotenvy::dotenv().ok();
 
     let _guard = obsrv::initialise()?;
 
-    tracing::error!("start");
+    let app = Router::new().route("/", get(handler));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    info!("listening on {addr}");
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .with_graceful_shutdown(io::quit::shutdown_signal())
+        .await?;
 
     Ok(())
+}
+
+async fn handler() -> &'static str {
+    "hello"
 }
