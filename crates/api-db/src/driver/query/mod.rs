6f8213dcfe8b5c5af9e49aae327ea17eb30de94a
rtkay123 <dev@kanjala.com>
2023-10-18 19:24:39 +0200

    feat: use db if cache deserialisation fails

--- crates/api-db/src/driver/query/mod.rs
+++ crates/api-db/src/driver/query/mod.rs
@@ -23,119 +23,139 @@ async fn validate_cache(database: &DatabaseConnection, cache_key: &str) -> Resul
         })
 }
 
+type ListResult = Result<Box<dyn ExactSizeIterator<Item = Category> + Send + Sync>, String>;
+
 #[async_trait]
 impl Query for DatabaseConnection {
-    async fn get_categories(
-        &self,
-    ) -> Result<Box<dyn ExactSizeIterator<Item = Category> + Send + Sync>, String> {
+    async fn get_categories(&self) -> ListResult {
         let cache_key = CacheKey::AllCategories.to_string();
 
+        let db_call = |cache_key: String| async {
+            self.surreal
+                .select(*CATEGORY_COLLECTION)
+                .await
+                .map_err(map_err)
+                .map(|items: Vec<InternalCategory>| async {
+                    let data: Vec<Category> = items.into_iter().map(Category::from).collect();
+
+                    let ex = self.cache_ttl;
+
+                    match bincode::serialize(&data) {
+                        Ok(data) => {
+                            redis_set(data, ex, cache_key, self.redis.clone()).await;
+                        }
+                        Err(e) => {
+                            tracing::error!("{e}");
+                        }
+                    }
+                    data.into_iter()
+                })
+        };
+
         match validate_cache(self, &cache_key).await {
-            Ok(ref val) => {
-                let cache_data: Vec<Category> = bincode::deserialize(val).map_err(map_err)?;
-                Ok(Box::new(cache_data.into_iter()))
-            }
+            Ok(ref val) => match bincode::deserialize::<Vec<Category>>(val).map_err(map_err) {
+                Ok(cache_data) => return Ok(Box::new(cache_data.into_iter())),
+                Err(e) => {
+                    // cache deserialisation failed, try from db
+                    tracing::error!(cache_key = cache_key, "{e}");
+                    let res = db_call(cache_key).await?.await;
+                    Ok(Box::new(res))
+                }
+            },
 
             Err(e) => {
-                tracing::warn!(cache_key = %cache_key, "{e}");
-                let items: Vec<InternalCategory> = self
-                    .surreal
-                    .select(*CATEGORY_COLLECTION)
-                    .await
-                    .map_err(map_err)?;
-
-                let mut item = items.into_iter().map(Category::from);
-
-                let data: Vec<Category> = item.by_ref().collect();
-
-                let ex = self.cache_ttl;
-
-                match bincode::serialize(&data) {
-                    Ok(data) => {
-                        redis_set(data, ex, cache_key, self.redis.clone()).await;
-                    }
-                    Err(e) => {
-                        tracing::error!("{e}");
-                    }
-                }
-
-                Ok(Box::new(data.into_iter()))
+                tracing::debug!(cache_key = cache_key, "cache invalid, {e}");
+                let res = db_call(cache_key).await?.await;
+                Ok(Box::new(res))
             }
         }
     }
 
     async fn get_category_by_id(&self, id: &str) -> Result<Option<Category>, String> {
-        let cache_key = CacheKey::Category { id }.to_string();
+        let id = id.to_owned();
+        let cache_key = CacheKey::Category { id: &id }.to_string();
+
+        let db_call = |id: String, cache_key: String| async move {
+            let id_internal = Thing::from_str(&id)
+                .map_err(|_| map_err(format!("id {id} could not be parsed")))?;
+
+            let category: Option<InternalCategory> =
+                self.surreal.select(id_internal).await.map_err(map_err)?;
+
+            if let Some(category) = category {
+                let category = Category::from(category);
+                let bytes = category.write_to_bytes().map_err(map_err)?;
+                let ex = self.cache_ttl;
+                redis_set(bytes, ex, cache_key, self.redis.clone()).await;
+                Ok::<Option<Category>, String>(Some(category))
+            } else {
+                Ok(None)
+            }
+        };
 
         match validate_cache(self, &cache_key).await {
-            Ok(bytes) => {
-                let category = Category::parse_from_bytes(&bytes).map_err(map_err)?;
-                Ok(Some(category))
-            }
+            Ok(bytes) => match Category::parse_from_bytes(&bytes).map_err(map_err) {
+                Ok(category) => Ok(Some(category)),
+                Err(e) => {
+                    tracing::error!(cache_key = cache_key, "{e}");
+                    db_call(id, cache_key).await
+                }
+            },
             Err(e) => {
                 tracing::warn!("{e}");
-                let id_internal = Thing::from_str(id)
-                    .map_err(|_| map_err(format!("id {id} could not be parsed")))?;
-
-                let category: Option<InternalCategory> =
-                    self.surreal.select(id_internal).await.map_err(map_err)?;
-
-                if let Some(category) = category {
-                    let category = Category::from(category);
-                    let bytes = category.write_to_bytes().map_err(map_err)?;
-                    let ex = self.cache_ttl;
-                    redis_set(bytes, ex, cache_key, self.redis.clone()).await;
-                    Ok(Some(category))
-                } else {
-                    Ok(None)
-                }
+                db_call(id, cache_key).await
             }
         }
     }
 
-    async fn get_sub_categories<'a>(
-        &self,
-        parent_id: Option<&'a str>,
-    ) -> Result<Box<dyn ExactSizeIterator<Item = Category> + Send + Sync>, String> {
+    async fn get_sub_categories<'a>(&self, parent_id: Option<&'a str>) -> ListResult {
         let cache_key = CacheKey::SubCategories {
             parent: parent_id.unwrap_or_default(),
         }
         .to_string();
 
-        match validate_cache(self, &cache_key).await {
-            Ok(ref val) => {
-                let cache_data: Vec<Category> = bincode::deserialize(val).map_err(map_err)?;
-                Ok(Box::new(cache_data.into_iter()))
+        let db_call = |cache_key: String| async move {
+            let mut resp = self
+                .surreal
+                .query(if let Some(parent) = parent_id {
+                    format!("SELECT sub_categories.*.* FROM {parent};")
+                } else {
+                    format!("SELECT * FROM {} WHERE is_root=true", *CATEGORY_COLLECTION)
+                })
+                .await
+                .map_err(map_err)?;
+
+            let items: Vec<InternalCategory> = resp.take(0).map_err(map_err)?;
+
+            let data: Vec<Category> = items.into_iter().map(Category::from).collect();
+
+            let ex = self.cache_ttl;
+            match bincode::serialize(&data) {
+                Ok(data) => {
+                    redis_set(data, ex, cache_key, self.redis.clone()).await;
+                }
+                Err(e) => {
+                    tracing::error!("{e}");
+                }
             }
 
-            Err(e) => {
-                tracing::warn!(cache_key = %cache_key, "{e}");
-                let mut resp = self
-                    .surreal
-                    .query(if let Some(parent) = parent_id {
-                        format!("SELECT sub_categories.*.* FROM {parent};")
-                    } else {
-                        format!("SELECT * FROM {} WHERE is_root=true", *CATEGORY_COLLECTION)
-                    })
-                    .await
-                    .map_err(map_err)?;
+            Ok::<Vec<Category>, String>(data)
+        };
 
-                let items: Vec<InternalCategory> = resp.take(0).map_err(map_err)?;
-
-                let mut item = items.into_iter().map(Category::from);
-                let data: Vec<Category> = item.by_ref().collect();
-
-                let ex = self.cache_ttl;
-                match bincode::serialize(&data) {
-                    Ok(data) => {
-                        redis_set(data, ex, cache_key, self.redis.clone()).await;
-                    }
-                    Err(e) => {
-                        tracing::error!("{e}");
-                    }
+        match validate_cache(self, &cache_key).await {
+            Ok(ref val) => match bincode::deserialize::<Vec<Category>>(val).map_err(map_err) {
+                Ok(cache_data) => Ok(Box::new(cache_data.into_iter())),
+                Err(e) => {
+                    tracing::error!(cache_key = cache_key, "{e}");
+                    let result = db_call(cache_key).await?;
+                    Ok(Box::new(result.into_iter()))
                 }
+            },
 
-                Ok(Box::new(data.into_iter()))
+            Err(e) => {
+                tracing::debug!(cache_key = %cache_key, "{e}");
+                let result = db_call(cache_key).await?;
+                Ok(Box::new(result.into_iter()))
             }
         }
     }
